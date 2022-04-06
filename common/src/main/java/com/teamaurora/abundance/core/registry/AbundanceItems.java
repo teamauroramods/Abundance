db9ab068583a58c46136c98749d8fe977ec96824
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-04-06 18:04:30 -0400

    Add lavender salad & sunflower seeds

--- common/src/main/java/com/teamaurora/abundance/core/registry/AbundanceItems.java
+++ common/src/main/java/com/teamaurora/abundance/core/registry/AbundanceItems.java
@@ -7,6 +7,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemNameBlockItem;
@@ -18,9 +19,11 @@ public class AbundanceItems {
 
     public static final Supplier<Item> LAVENDER = registerItem("lavender", () -> new ItemNameBlockItem(AbundanceBlocks.LAVENDER.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
     public static final Supplier<Item> LAVENDER_TEA = registerItem("lavender_tea", () -> new DrinkItem(new Item.Properties().food(Foods.LAVENDER_TEA).stacksTo(16).tab(CreativeModeTab.TAB_FOOD)));
+    public static final Supplier<Item> LAVENDER_SALAD = registerItem("lavender_salad", () -> new BowlFoodItem(new Item.Properties().food(Foods.LAVENDER_SALAD).stacksTo(1).tab(CreativeModeTab.TAB_FOOD)));
 
     public static final Supplier<Item> CHICORY_ROOTS = registerItem("chicory_roots", () -> new Item(new Item.Properties().food(Foods.CHICORY_ROOTS).tab(CreativeModeTab.TAB_FOOD)));
     public static final Supplier<Item> PINK_BLOSSOMS = registerItem("pink_blossoms", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final Supplier<Item> SUNFLOWER_SEEDS = registerItem("sunflower_seeds", () -> new Item(new Item.Properties().food(Foods.SUNFLOWER_SEEDS).tab(CreativeModeTab.TAB_FOOD)));
 
     public static Supplier<Item> registerItem(String id, Supplier<Item> item) {
         Supplier<Item> register = ITEMS.register(id, item);
@@ -30,5 +33,7 @@ public class AbundanceItems {
     public static final class Foods {
         public static final FoodProperties LAVENDER_TEA = new FoodProperties.Builder().nutrition(0).saturationMod(0.0F).alwaysEat().effect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 600, 1), 1.0F).effect(new MobEffectInstance(MobEffects.REGENERATION, 100, 1), 1.0F).build();
         public static final FoodProperties CHICORY_ROOTS = new FoodProperties.Builder().nutrition(2).build();
+        public static final FoodProperties LAVENDER_SALAD = new FoodProperties.Builder().nutrition(6).saturationMod(0.5F).effect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 2400), 1.0F).effect(new MobEffectInstance(MobEffects.REGENERATION, 200), 1.0F).build();
+        public static final FoodProperties SUNFLOWER_SEEDS = new FoodProperties.Builder().nutrition(2).saturationMod(0.1F).fast().build();
     }
 }
